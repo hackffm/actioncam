3510e6705831ac1147c7632e7b73dbcaa9d7403b
vkuehn <vkuehn@more-machines.org>
2018-12-21 16:10:22 +0000

    new shell scripts for setup and start of actioncam

---
+++ shell/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update && sudo apt dist-upgrade
+sudo apt install python3-venv
+python3 -m pip install --upgrade pip
+if [ ! -d ../venv ]; then
+	mkdir ../venv
+	python3 -m venv ../venv
+fi
+alias activate=". ../.venv/bin/activate"
