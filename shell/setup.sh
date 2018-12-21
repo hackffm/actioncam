1ba8acb1bd8087bc09cbccc52d1377e4f3b412e9
vkuehn <vkuehn@more-machines.org>
2018-12-21 17:43:13 +0000

    reworked steps

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 sudo apt update && sudo apt dist-upgrade
-sudo apt install python3-venv
+sudo apt install python3-venv opencv*
 python3 -m pip install --upgrade pip
 if [ ! -d ../venv ]; then
 	mkdir ../venv
 	python3 -m venv ../venv
 fi
-alias activate=". ../.venv/bin/activate"
