8e72f6c7a7eaa6f340b3a92f9199d35e5a707588
vkuehn <vkuehn@more-machines-org>
2018-12-30 20:17:20 +0000

    fix setup bug when extracting opencv

--- shell/setup.sh
+++ shell/setup.sh
@@ -23,12 +23,11 @@ if [ ! -d ../temp ]; then
 fi
 echo 'get opencv 4.0'
 # thanks to adrian https://www.pyimagesearch.com/2017/09/04/raspbian-stretch-install-opencv-3-python-on-your-raspberry-pi/
-if [ ! -d ~/opencv.zip ]; then
+if [ ! -f ~/opencv.zip ]; then
 	cd ~
-	wget -O opencv.tar.gz https://github.com/opencv/opencv/archive/4.0.0.zip
-	wget -O opencv_contrip.tar.gz https://github.com/opencv/opencv_contrib/archive/4.0.0.zip
+	wget -O opencv.zip https://github.com/opencv/opencv/archive/4.0.0.zip
+	wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.0.0.zip
 fi
+cd ~
 unzip opencv.zip
 unzip opencv_contrib.zip
-mv opencv opencv-4.0.0
-mv opencv_contrib opencv_contrib-4.0.0
