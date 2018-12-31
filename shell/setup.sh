e7cdfe908aea653bed2767d91354f92d50afb085
vkuehn <vkuehn>
2018-12-31 10:27:34 +0100

    add own copy section for opencv_contrib.zip

--- shell/setup.sh
+++ shell/setup.sh
@@ -28,6 +28,10 @@ if [ ! -f ~/opencv.zip ]; then
 	wget -O opencv.zip https://github.com/opencv/opencv/archive/4.0.0.zip
 	wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.0.0.zip
 fi
+if [ ! -f ~/opencv_contrib.zip ]; then
+	cd ~
+	wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.0.0.zip
+fi
 cd ~
 unzip opencv.zip
 unzip opencv_contrib.zip
