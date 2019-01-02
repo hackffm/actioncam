b07795f46e6099437edc2e2b683fe1c47c59878e
vkuehn <vkuehn@more-machines.org>
2019-01-02 18:03:44 +0100

    setup.sh for new log and recording paths amended

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 sudo apt update && sudo apt dist-upgrade
 sudo apt autoremove && sudo apt autoclean
-sudo apt install -y python3-venv opencv*
+sudo apt install -y python3-dev python3-venv
 sudo apt install -y build-essential cmake unzip pkg-config
+sudo apt install -y opencv*
 sudo apt install -y libjpeg-dev libtiff-dev libjasper-dev libpng-dev
 sudo apt install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt install -y libxvidcore-dev libx264-dev
 sudo apt install -y libgtk-3-dev libcanberra-gtk*
 sudo apt install -y libatlas-base-dev gfortran
-sudo apt install -y python3-dev
 python3 -m pip install --upgrade pip
 if [ ! -d ../venv ]; then
 	mkdir ../venv
@@ -18,8 +18,10 @@ echo 'set venv'
 actioncam_dir='/home/pi/git/actioncam/'
 source $actioncam_dir'venv/bin/activate'
 
-if [ ! -d ../temp ]; then
-	mkdir ../temp
+if [ ! -d ~/actioncam ]; then
+	mkdir ~/actioncam
+    mkdir ~/actioncam/log
+    mkdir  ~/actioncam/recording
 fi
 echo 'get opencv 4.0'
 # thanks to adrian https://www.pyimagesearch.com/2017/09/04/raspbian-stretch-install-opencv-3-python-on-your-raspberry-pi/
