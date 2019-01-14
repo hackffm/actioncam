5a059f02ae45bf745942c4a0e419f8a5472fc675
vkuehn <vkuehn@more-machines.org>
2019-01-14 20:08:29 +0100

    set download folder for conda script to ~/temp
    Former-commit-id: 32d500182ce208ee520a97bb6b143c35b5662b09

--- shell/setup/conda_install_x86_32.sh
+++ shell/setup/conda_install_x86_32.sh
@@ -1,6 +1,6 @@
 if [ ! -d ~/temp ]; then
     mkdir ~/temp
 fi
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86.sh ~/temp
-chmod u+x Miniconda3-latest-Linux-x86.sh
-./Miniconda3-latest-Linux-x86.sh -b -p /home/pi/miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86.sh -P ~/temp
+chmod u+x ~/temp/Miniconda3-latest-Linux-x86.sh
+~/temp/Miniconda3-latest-Linux-x86.sh -b -p /home/pi/miniconda
