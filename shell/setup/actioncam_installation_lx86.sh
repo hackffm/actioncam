6599e348cf5fbde6ae7f8c5b781191130b19c12e
vkuehn <vkuehn@more-machines.org>
2019-01-14 18:40:19 +0100

    start creating fully silent installation

---
+++ shell/setup/actioncam_installation_lx86.sh
@@ -0,0 +1,6 @@
+./setup_debs.sh
+./setup_folders.sh
+./conda_install_x86_32.sh
+
+source ~/miniconda/bin/activate
+python -V
--- shell/setup/setup_folders.sh
+++ shell/setup/setup_folders.sh
@@ -4,4 +4,6 @@ if [ ! -d ~/actioncam ]; then
 	mkdir ~/actioncam
 	mkdir ~/actioncam/log
 	mkdir ~/actioncam/recording
+
+    cp ../../code/config.json ~/actioncam
 fi
