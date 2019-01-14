32d500182ce208ee520a97bb6b143c35b5662b09
vkuehn <vkuehn@more-machines.org>
2019-01-14 20:08:29 +0100

    set download folder for conda script to ~/temp

--- shell/setup/conda_install_arm71.sh
+++ shell/setup/conda_install_arm71.sh
@@ -1,3 +1,6 @@
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-arm71.sh
-chmod u+x Miniconda3-latest-Linux-arm71.sh
-./Miniconda3-latest-Linux-arm71.sh -b -p /home/pi/miniconda
+if [ ! -d ~/temp ]; then
+    mkdir ~/temp
+fi
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-arm71.sh -P ~/temp
+chmod u+x ~/temp/Miniconda3-latest-Linux-arm71.sh
+~temp/Miniconda3-latest-Linux-arm71.sh -b -p ~/miniconda
