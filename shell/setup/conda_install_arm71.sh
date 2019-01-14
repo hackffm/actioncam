77a019829b065202bb677d5eb82eb0b818e7ed63
vkuehn <vkuehn@more-machines.org>
2019-01-14 20:23:00 +0100

    install file name changed

--- shell/setup/conda_install_arm71.sh
+++ shell/setup/conda_install_arm71.sh
@@ -2,5 +2,5 @@ if [ ! -d ~/temp ]; then
     mkdir ~/temp
 fi
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-armv7l.sh -P ~/temp
-chmod u+x ~/temp/Miniconda3-latest-Linux-arm71.sh
-~temp/Miniconda3-latest-Linux-arm71.sh -b -p ~/miniconda
+chmod u+x ~/temp/Miniconda3-latest-Linux-armv7l.sh
+~temp/Miniconda3-latest-Linux-armv7l.sh -b -p ~/miniconda
