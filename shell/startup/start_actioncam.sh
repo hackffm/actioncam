1bcf8fbf4abf18267e3f0e04a7b2b1d0beb71a6b
vkuehn <vkuehn@more-machines.org>
2019-01-14 18:00:35 +0000

    setup bug fixes
    Former-commit-id: 82e2d7f680929f64912af4239135a4115e4134f5

--- shell/startup/start_actioncam.sh
+++ shell/startup/start_actioncam.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source ~/miniconda/bin/activate
 # to be discussed if the folder path stays like that
-actioncam_dir='/home/pi/git/actioncam/'
+actioncam_dir='../../'
 cd $actioncam_dir'code'
-python $actioncam_dir'code/actioncam.py'
+python actioncam.py
 
