6211960b39c3bc4548cd90e2475076977d1fc56a
vkuehn <vkuehn@more-machines.org>
2019-01-14 20:16:33 +0100

    change https to http download
    Former-commit-id: a16d3bae71c72262d6d9eff7cc3d93ebb4811278

--- shell/setup/conda_install_arm71.sh
+++ shell/setup/conda_install_arm71.sh
@@ -1,6 +1,6 @@
 if [ ! -d ~/temp ]; then
     mkdir ~/temp
 fi
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-arm71.sh -P ~/temp
+wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-arm71.sh -P ~/temp
 chmod u+x ~/temp/Miniconda3-latest-Linux-arm71.sh
 ~temp/Miniconda3-latest-Linux-arm71.sh -b -p ~/miniconda
