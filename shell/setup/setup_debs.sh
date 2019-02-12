f104900b44559b59ad96c0476e0266c42cd0125c
vkuehn <vkuehn@more-machines.org>
2019-02-12 18:58:45 +0100

    twek to setup for opencv compilation

--- shell/setup/setup_debs.sh
+++ shell/setup/setup_debs.sh
@@ -9,4 +9,7 @@ sudo apt --yes install  libxvidcore-dev libx264-dev
 sudo apt --yes install  libgtk-3-dev libcanberra-gtk*
 sudo apt --yes install  libatlas-base-dev gfortran
 sudo apt --yes install  python3-dev
+sudo apt --yes install libgstreamer-plugins-base1.0-dev
+sudo apt --yes install libgflags* python3-gflags python-gflags python3-google*
+sudo apt --yes install libgoogl* libeigen3* python3-minieigen
 sudo apt autoremove && sudo apt autoclean
