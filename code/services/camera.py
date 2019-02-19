49dfef2b1ac33b1a8d5b1aea08b6fdbc68f4d6ca
vkuehn <vkuehn@more-machines.org>
2019-02-19 20:48:30 +0100

    reduce double code

--- code/services/camera.py
+++ code/services/camera.py
@@ -57,9 +57,35 @@ class Camera:
         return fName
 
     def file_name_video(self, type):
-        fName = self.configuration.output_folder() + '/' + self.default['identify'] + '_' + self.config_output['file_name_' + type] + '_' \
+        f_name = self.configuration.output_folder() + '/' + self.default['identify'] + '_' + self.config_output['file_name_' + type] + '_' \
                 + self.now + self.config_output['file_extension']
-        return fName
+        return f_name
+
+    def frame_draw_detections(self, frame, first_frame, gray):
+        detected = False
+        frame_delta = cv2.absdiff(first_frame, gray)
+        text = self.text_detected_false()
+        thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.dilate(thresh, None, iterations=2)
+
+        (_, contours, _) = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for c in contours:
+            if cv2.contourArea(c) < self.min_area:
+                continue
+            # detected
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            text = self.text_detected()
+            detected = True
+
+        cv2.putText(frame, "Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        return frame, detected
+
+    def frame_grey(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+        return gray
 
     def log(self, text):
         self.helper.log_add_text(self.name, text)
@@ -90,50 +116,24 @@ class Camera:
         cap, out = self.capture_config(type_motion)
 
         if cap is False or out is False:
-            self.log('failed to initialse camera')
+            self.log('failed to initialise camera')
             return
 
-        detected = False
         first_frame = None
-        min_area = 500
         preview = True
 
         while duration >= 1:
             (grabbed, frame) = cap.read()
-            text = "not detected"
 
             if not grabbed:
+                self.log('failed to grab camera')
                 break
 
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
+            gray = self.frame_grey(frame)
             if first_frame is None:
                 first_frame = gray
                 continue
-
-            frame_delta = cv2.absdiff(first_frame, gray)
-            thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
-
-            thresh = cv2.dilate(thresh, None, iterations=2)
-            (_, contours, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-            for c in contours:
-                if cv2.contourArea(c) < min_area:
-                    continue
-                # detected
-                (x, y, w, h) = cv2.boundingRect(c)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                text = self.text_detected()
-                detected = True
-
-            cv2.putText(frame, "Status: {}".format(text), (10, 20),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-            '''
-            not sure if still needed
-            cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-                        (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-            '''
+            frame, detected = self.frame_draw_detections(frame, first_frame, gray)
 
             if detected:
                 if preview:
@@ -142,7 +142,6 @@ class Camera:
                     self.log('preview ' + _image_name)
                     preview = False
                 out.write(frame)
-                text = self.text_detected_false()
 
             duration -= 1
             # end detection loop
@@ -158,11 +157,10 @@ class Camera:
         cap, out = self.capture_config(type_record)
 
         if cap is False or out is False:
-            self.log('failed to initialse camera')
+            self.log('failed to initialise camera')
             return
 
         recording = True
-
         ret, frame = cap.read()
 
         _image_name = self.file_name_image(type_record)
@@ -195,38 +193,17 @@ class Camera:
             return
 
         first_frame = None
-
         try:
             while duration >= 1:
                 (grabbed, frame) = cap.read()
                 if not grabbed:
+                    self.log('failed to grab camera')
                     break
-
-                text = self.text_detected_false()
-
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
+                gray = self.frame_grey(frame)
                 if first_frame is None:
                     first_frame = gray
                     continue
-
-                frame_delta = cv2.absdiff(first_frame, gray)
-                thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
-                thresh = cv2.dilate(thresh, None, iterations=3)
-
-                (_, contours, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-                for c in contours:
-                    if cv2.contourArea(c) < self.min_area:
-                        continue
-                    # detected
-                    (x, y, w, h) = cv2.boundingRect(c)
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                    text = self.text_detected()
-
-                cv2.putText(frame, "Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
+                frame, detected = self.frame_draw_detections(frame, first_frame, gray)
                 self.queue_write(frame)
                 duration -= 1
                 # end detection loop
@@ -234,7 +211,7 @@ class Camera:
             self.log('showed video')
         except Exception as e:
             self.log(str(e))
-        # end show
+        # end show_video
         return
 
     # -- main ----------------------------------------------------------------
