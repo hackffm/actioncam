84f8a77d97feb6562b4d2076370a96194d9e2aff
vkuehn <vkuehn@more-machines.org>
2019-08-17 19:05:39 +0200

    added camera test...early version

---
+++ code/test/test_camera_usb_0.py
@@ -0,0 +1,54 @@
+# Python programe to illustrate
+# simple thresholding type on an image
+
+# organizing imports
+import cv2
+import numpy as np
+
+
+def capture_config():
+    frame_height = 480
+    frame_rate = 20
+    frame_width = 640
+
+    _input = 'usb_0'
+    cap = cv2.VideoCapture(_input)
+    cap.set(3, frame_width)
+    cap.set(4, frame_height)
+    if not cap.isOpened():
+        print('Unable to read camera feed')
+        return False
+    return cap
+
+# path to input image is specified and
+# image is loaded with imread command
+cap = capture_config()
+image1 = cap.read()
+
+# cv2.cvtColor is applied over the
+# image input with applied parameters
+# to convert the image in grayscale
+img = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+
+# applying different thresholding
+# techniques on the input image
+# all pixels value above 120 will
+# be set to 255
+ret, thresh1 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
+ret, thresh2 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY_INV)
+ret, thresh3 = cv2.threshold(img, 120, 255, cv2.THRESH_TRUNC)
+ret, thresh4 = cv2.threshold(img, 120, 255, cv2.THRESH_TOZERO)
+ret, thresh5 = cv2.threshold(img, 120, 255, cv2.THRESH_TOZERO_INV)
+
+# the window showing output images
+# with the corresponding thresholding
+# techniques applied to the input images
+cv2.imshow('Binary Threshold', thresh1)
+cv2.imshow('Binary Threshold Inverted', thresh2)
+cv2.imshow('Truncated Threshold', thresh3)
+cv2.imshow('Set to 0', thresh4)
+cv2.imshow('Set to 0 Inverted', thresh5)
+
+# De-allocate any associated memory usage
+if cv2.waitKey(0) & 0xff == 27:
+    cv2.destroyAllWindows()
