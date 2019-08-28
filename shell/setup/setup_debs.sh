d09ace90d3abc35944d78fa9ab98e2930d602cbf
vkuehn <vkuehn@more-machines.org>
2019-08-28 23:08:36 +0200

    add missing library for stretch

--- shell/setup/setup_debs.sh
+++ shell/setup/setup_debs.sh
@@ -13,3 +13,4 @@ sudo apt --yes install libgstreamer-plugins-base1.0-dev
 sudo apt --yes install libgflags* python3-gflags python-gflags python3-google*
 sudo apt --yes install libgoogl* libeigen3* python3-minieigen
 sudo apt autoremove && sudo apt autoclean
+sudo apt --yes install libhdf5-dev libhdf5-serial-dev
