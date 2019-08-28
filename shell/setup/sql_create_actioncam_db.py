e192030c01f216bf301e5e0cf20089917fdab670
vkuehn <vkuehn@more-machines.org>
2019-08-29 00:48:11 +0200

    create and test db

--- shell/setup/sql_create_actioncam_db.py
+++ shell/setup/sql_create_actioncam_db.py
@@ -12,12 +12,28 @@ def dir_data():
 dd = dir_data()
 db = dd + '/actioncam.db'
 conn = sqlite3.connect(db)
-
 print ("database successfully creates as" + db)
+
 conn.execute('''CREATE TABLE COMPRESS (
                 ID INT PRIMARY KEY     NOT NULL,
                 NAME           TEXT    NOT NULL,
                 DATE           TEXT    NOT NULL
                 );''')
 print ("table compress created successfully")
+
+conn.execute('''CREATE TABLE RECORDING (
+                ID INT PRIMARY KEY     NOT NULL,
+                NAME           TEXT    NOT NULL,
+                DATE           TEXT    NOT NULL
+                );''')
+print ("table recording created successfully")
+
+#connection tables
+conn.execute('''CREATE TABLE COMPRESS2RECORDING (
+                ID INT PRIMARY KEY     NOT NULL,
+                ID_COMPRESS    INT     NOT NULL,
+                ID_RECORDING   INT     NOT NULL
+                );''')
+print ("table compress2recording created successfully")
+
 conn.close()
