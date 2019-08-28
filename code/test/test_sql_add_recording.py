13894ad569075d8cdce3a6f29b00fd3307d63c96
vkuehn <vkuehn@more-machines.org>
2019-08-29 00:49:36 +0200

    add missing test

---
+++ code/test/test_sql_add_recording.py
@@ -0,0 +1,26 @@
+import os
+import sqlite3
+import sys
+
+def db_path():
+    home = os.getenv('HOME')
+    path_data = home + '/actioncam/data/actioncam.db'
+    return path_data
+
+def db_exists(path_db):
+    if not os.path.exists(path_db):
+        return False
+    return True
+
+dp = db_path()
+if not db_exists(dp):
+    print('can not find db ' + dp)
+    sys.exit(1)
+
+conn = sqlite3.connect(dp)
+print('successfully connected to ' + dp)
+
+conn.execute("INSERT INTO RECORDING (ID,NAME,DATE) \
+        VALUES (1, '20190828225930', 20190828 )");
+
+
