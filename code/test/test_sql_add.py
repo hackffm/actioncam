0029e1b064307d2bb55b540d90a5bac5358e0420
vkuehn <vkuehn@more-machines.org>
2019-09-05 08:29:35 +0200

    avoid double code. add debug switch

--- code/test/test_sql_add.py
+++ code/test/test_sql_add.py
@@ -16,3 +16,4 @@ database = Database(configuration, helper)
 
 print(database.db_check())
 print(database.add_compressed2recording('20190818172955.zip', '1_record_20190818144842.jpeg'))
+#print(database.add_recording('1_record_20190818144842.jpeg'))
