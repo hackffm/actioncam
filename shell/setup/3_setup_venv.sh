a6635790398b9746d650bd8dff2b79c185ae4ee7
vkuehn <vkuehn@more-machines.org>
2019-12-27 10:37:24 +0000

    rework setup

--- shell/setup/3_setup_venv.sh
+++
@@ -1,6 +0,0 @@
-echo create additional venv folder
-sudo pip3 install --upgrade pip
-if [ ! -d ~/actioncam/venv ]; then
-	mkdir ~/actioncam/venv
-	python3 -m venv ~/actioncam/venv
-fi
--- shell/setup/setup.sh
+++ shell/setup/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo install debian packages
 DEBIAN_FRONTEND=noninteractive
 sudo apt update && sudo apt --yes dist-upgrade
 sudo apt --yes install  python3-venv
@@ -14,3 +15,25 @@ sudo apt --yes install libgflags* python3-gflags python-gflags python3-google*
 sudo apt --yes install libgoogl* libeigen3* python3-minieigen
 sudo apt --yes install libhdf5-dev libhdf5-serial-dev
 sudo apt --yes autoremove && sudo apt --yes autoclean
+#
+echo create Folders
+if [ ! -d ~/actioncam ]; then
+  echo "create actioncam folders"
+	mkdir ~/actioncam
+	mkdir ~/actioncam/data
+	mkdir ~/actioncam/log
+	mkdir ~/actioncam/recording
+	mkdir ~/actioncam/venv
+	echo "prepare config"
+  cp config.json ~/actioncam
+  sed -i -e 's/pi/'"${USER}"'/g' ~/actioncam/config.json
+fi
+#
+echo create python venv
+python3 -m venv ~/actioncam/venv
+
+echo install python packages
+source ~/actioncam/venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
