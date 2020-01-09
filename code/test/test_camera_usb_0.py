374b21e45bf0a2cc45d2a2953fca899901d0c054
vkuehn <vkuehn@more-machines.org>
2020-01-09 20:54:22 +0100

    simplify cv2 camera test

--- code/test/test_camera_usb_0.py
+++ code/test/test_camera_usb_0.py
@@ -1,9 +1,4 @@
-# simple  image capture
-
-# organizing imports
 import cv2
-import sys
-
 
 print('cv2 version is ' + str(cv2.getVersionString()))
 
@@ -11,21 +6,23 @@ print('cv2 version is ' + str(cv2.getVersionString()))
 def capture_config():
     vc = cv2.VideoCapture(0)
     if not vc.isOpened():
-        print('Unable to read camera feed')
         return False
     return vc
 
 
-cap = capture_config()
-if not cap:
-    sys.exit()
-(grabbed, frame) = cap.read()
-grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+while True:
+    cap = capture_config()
+    if not cap:
+        print('Unable to read camera feed')
+        break
+    grabbed, frame = cap.read()
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('captured frame', frame)
-cv2.imshow('greyed frame', grey)
+    cv2.imshow('captured frame', frame)
+    cv2.imshow('greyed frame', grey)
 
-# De-allocate any associated memory usage
-if cv2.waitKey(0) & 0xff == 27:
-    cap.release()
-    cv2.destroyAllWindows()
+    if cv2.waitKey(0) & 0xff == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
