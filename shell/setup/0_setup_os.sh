d8c502b904d033ea58df04ad6c33f7753c19a7ec
vkuehn <vkuehn@more-machines.org>
2023-08-31 18:02:13 +0200

    first start

--- shell/setup/0_setup_os.sh
+++ shell/setup/0_setup_os.sh
@@ -10,10 +10,8 @@ sudo apt install --yes libsz2 libharfbuzz0b
 sudo apt install --yes libqtgui4 libqt4-test libqtcore4
 sudo apt install --yes libgslcblas0 libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev 
 sudo apt install --yes python3-opencv python3-cv-bridge libopencv-dev
-sudo apt install --yes build-essential cmake git pkg-config libgtk-3-dev "libcanberra-gtk*"
+sudo apt install --yes build-essential cmake pkg-config libgtk-3-dev "libcanberra-gtk*"
 sudo apt install --yes libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev
 sudo apt install --yes libjpeg-dev libpng-dev libtiff-dev gfortran openexr libatlas-base-dev opencl-headers
 sudo apt install --yes python3-dev python3-numpy libtbb2 libtbb-dev libdc1394-22-dev
 sudo apt install --yes python3-venv python3-pip
-sudo apt install --yes gcc g++ make
-sudo apt --yes autoremove && sudo apt --yes autoclean
