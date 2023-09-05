43814a4b4aec0c8b1f826c7e99c0ef5b33960d8b
vkuehn <vkuehn@more-machines.org>
2023-09-05 22:50:47 +0200

    user more actioncam dir

--- shell/startup/start_actioncam.sh
+++ shell/startup/start_actioncam.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-source ~/actioncam/venv/bin/activate
-# to be discussed if the folder path stays like that
-actioncam_dir='../../'
-cd $actioncam_dir'code'
+actioncam_dir='~/actioncam'
+source "${actioncam_dir}/venv/bin/activate"
+cd "${actioncam_dir}/code"
 python actioncam.py
